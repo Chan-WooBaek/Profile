25e0ab1412335fd1636c648165435336e885ded4
Chan Baek <z5259628@ad.unsw.edu.au>
2024-03-17 03:33:40 +1100

    Update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,18 +9,21 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   metadataBase: new URL('http://localhost:3000'),
-  title: 'Title webtsite',
-  description: 'this is the desciption',
+  title: 'Chan Woo Baek',
+  description: 'My Portfolio',
+  icons: {
+    icon: '/favicon/favicon.ico'
+  },
   openGraph: {
-    title: 'Title webtsite',
-    description: 'this is the desciption',
+    title: 'Chan Woo Baek',
+    description: 'My Portfolio',
     image: 'url/image.png'
   },
   twitter: {
     card: 'summary_large_image',
-    site: '@eMartiiin94',
-    title: 'Title webtsite',
-    description: 'this is the desciption',
+    site: '@hello',
+    title: 'Chan Woo Baek',
+    description: 'My Portfolio',
     image: 'url/image.png'
   }
 }
@@ -32,39 +35,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicon/safari-pinned-tab.svg"
-          color="#000000"
-        />
-        <link rel="shortcut icon" href="/favicon/favicon.ico" />
-        <meta name="msapplication-TileColor" content="#000000" />
-        <meta
-          name="msapplication-config"
-          content="/favicon/browserconfig.xml"
-        />
-        <meta name="theme-color" content="#000" />
-        <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      </head>
       <body className={inter.className}>
         <div className="min-h-screen">{children}</div>
         <Footer />
