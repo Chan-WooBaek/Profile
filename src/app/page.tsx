0c9fe33d3bd93de6446d14b08f8ec138989cc275
Chan Baek <z5259628@ad.unsw.edu.au>
2024-03-19 15:45:39 +1100

    Remove background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import Container from "@/app/_components/container";
 import { Intro } from "./_components/intro";
+import { FloatingNav } from "./_components/ui/floating-navbar";
 
 export default function Index() {
   return (
     <main>
+      <FloatingNav navItems={[{name: 'Home', link: ''}]}/>
+      {/* <div className="flex flex-col pt-12 pr-32"> 
+        <div className="flex border border-black self-end rounded-lg py-1 px-4">
+          <a href={''} target="__blank">
+            <p className=" font-sans">Contact</p>
+          </a>
+        </div>
+      </div> */}
       <Intro/>
       <Container>
-        
       </Container>
     </main>
   );
